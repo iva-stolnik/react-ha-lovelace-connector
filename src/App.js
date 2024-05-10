dedbf7ce54cf0bf1a03b5eac33391f7e46407389
Iva Stolnik <iva.stolnik91@gmail.com>
2024-05-10 17:34:04 +0200

    add callService and callWS examples

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+  // Method to call a service
+  callService = (domain, service, serviceData) => {
+    if (this.props.hass && this.props.hass.callService) {
+      this.props.hass.callService(domain, service, serviceData);
+    } else {
+      console.error("Home Assistant instance (hass) or callService method is not available.");
+    }
+  }
+  // Method to call a WebSocket
+  callWebSocket = (type,domain,service,target,serviceData) => {
+    if (this.props.hass && this.props.hass.callWS) {
+      const message = {
+          type: type,
+          domain: domain,  
+          service: service, 
+          target: target,
+          service_data: serviceData 
+      };
+      this.props.hass.callWS(message);
+    }
+    else {
+      console.error("Home Assistant instance (hass) or callWS method is not available.");
+    }
+  }
+
   render() {
     const { config, hass } = this.props;
 
     const configText = config ? JSON.stringify(config) : 'No configuration provided';
-    const hassText = hass ? JSON.stringify(hass) : 'No hass data provided';
+    const hassText = hass ? 'HA connected' : 'No hass data provided';
 
     return (
       <div>
         <h1>Configuration</h1>
         <pre>{configText}</pre>
-        <h2>Status</h2>
-        <pre>{hassText}</pre>
+        <h2>Status: {hassText}</h2>
+        <div>
+          <button onClick={() => this.callService('light', 'toggle', { entity_id: 'light.wl_office_1' })}>
+            Toggle Light (call service)
+          </button>
+        </div>
+        <div>
+          <button onClick={() => this.callWebSocket('call_service', 'light', 'toggle', { entity_id: 'light.wl_office_1' })}>
+            Toggle Light (call websocket)
+          </button>
+        </div>
       </div>
     );
   }
